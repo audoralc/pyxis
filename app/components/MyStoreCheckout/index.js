7e8a0eb839b03ea9c21eb17f63a78f7bc13fc7f0
Oscar-21 <austin.conder@outlook.com>
2017-05-31 13:42:40 -0400

    added form components and stripe integration

---
+++ app/components/MyStoreCheckout/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Elements } from 'react-stripe-elements';
+import CheckoutForm from 'components/CheckoutForm';
+
+class MyStoreCheckout extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <Elements>
+          <CheckoutForm />
+        </Elements>
+      </div>
+    );
+  }
+}
+export default MyStoreCheckout;
+
