ce1812ff0b79e52d2cc615287d1ba1b0769a80e8
Oscar-21 <austin.conder@outlook.com>
2017-05-31 14:17:42 -0400

    updated Subscribe container

--- app/containers/Subscribe/index.js
+++ app/containers/Subscribe/index.js
@@ -6,15 +6,19 @@
 
 import React from 'react';
 import Helmet from 'react-helmet';
+import { StripeProvider } from 'react-stripe-elements';
+import MyStoreCheckout from 'components/MyStoreCheckout';
 
 export default class Subscribe extends React.PureComponent {
   render() {
     return (
       <div>
         <Helmet title="Subscribe" meta={[ { name: 'description', content: 'Description of Subscribe' }]}/>
-
-      //Remove this line and you can start writing your code here.
+        <StripeProvider apiKey="pk_test_rJKqFRMRduGAyguxdWT2TfcI">
+          <MyStoreCheckout />
+        </StripeProvider>
       </div>
     );
   }
 }
+
