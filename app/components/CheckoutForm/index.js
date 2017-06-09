bfd62e75d7b596ebbcd993aef3244e2915085b44
Oscar-21 <austin.conder@outlook.com>
2017-06-08 21:44:05 -0400

    checkout form functional
    fully integrated with backend

--- app/components/CheckoutForm/index.js
+++ app/components/CheckoutForm/index.js
@@ -3,299 +3,69 @@
 */
 import React from 'react';
 import { injectStripe } from 'react-stripe-elements';
-// import AddressSection from 'components/AddressSection';
 import CardSection from 'components/CardSection';
-import RaisedButton from 'material-ui/RaisedButton';
 import MenuItem from 'material-ui/MenuItem';
-import TextField from 'material-ui/TextField';
-import { orange500, blue500 } from 'material-ui/styles/colors';
 import SelectField from 'material-ui/SelectField';
-/* import CountryList from './CountryList.json'; */
-
-const countryDropdown = [];
-const countries = [
-  'Afghanistan',
-  'Åland Islands',
-  'Albania',
-  'Algeria',
-  'American Samoa',
-  'Andorra',
-  'Angola',
-  'Anguilla',
-  'Antarctica',
-  'Antigua and Barbuda',
-  'Argentina',
-  'Armenia',
-  'Aruba',
-  'Australia',
-  'Austria',
-  'Azerbaijan',
-  'Bahamas',
-  'Bahrain',
-  'Bangladesh',
-  'Barbados',
-  'Belarus',
-  'Belgium',
-  'Belize',
-  'Benin',
-  'Bermuda',
-  'Bhutan',
-  'Bolivia, Plurinational State of',
-  'Bonaire, Sint Eustatius and Saba',
-  'Bosnia and Herzegovina',
-  'Botswana',
-  'Bouvet Island',
-  'Brazil',
-  'British Indian Ocean Territory',
-  'Brunei Darussalam',
-  'Bulgaria',
-  'Burkina Faso',
-  'Burundi',
-  'Cambodia',
-  'Cameroon',
-  'Canada',
-  'Cape Verde',
-  'Cayman Islands',
-  'Central African Republic',
-  'Chad',
-  'Chile',
-  'China',
-  'Christmas Island',
-  'Cocos (Keeling) Islands',
-  'Colombia',
-  'Comoros',
-  'Congo',
-  'Congo, the Democratic Republic of the',
-  'Cook Islands',
-  'Costa Rica',
-  'Côte d\'Ivoire',
-  'Croatia',
-  'Cuba',
-  'Curaçao',
-  'Cyprus',
-  'Czech Republic',
-  'Denmark',
-  'Djibouti',
-  'Dominica',
-  'Dominican Republic',
-  'Ecuador',
-  'Egypt',
-  'El Salvador',
-  'Equatorial Guinea',
-  'Eritrea',
-  'Estonia',
-  'Ethiopia',
-  'Falkland Islands (Malvinas)',
-  'Faroe Islands',
-  'Fiji',
-  'Finland',
-  'France',
-  'French Guiana',
-  'French Polynesia',
-  'French Southern Territories',
-  'Gabon',
-  'Gambia',
-  'Georgia',
-  'Germany',
-  'Ghana',
-  'Gibraltar',
-  'Greece',
-  'Greenland',
-  'Grenada',
-  'Guadeloupe',
-  'Guam',
-  'Guatemala',
-  'Guernsey',
-  'Guinea',
-  'Guinea-Bissau',
-  'Guyana',
-  'Haiti',
-  'Heard Island and McDonald Islands',
-  'Holy See (Vatican City State)',
-  'Honduras',
-  'Hong Kong',
-  'Hungary',
-  'Iceland',
-  'India',
-  'Indonesia',
-  'Iran, Islamic Republic of',
-  'Iraq',
-  'Ireland',
-  'Isle of Man',
-  'Israel',
-  'Italy',
-  'Jamaica',
-  'Japan',
-  'Jersey',
-  'Jordan',
-  'Kazakhstan',
-  'Kenya',
-  'Kiribati',
-  'Korea, Democratic People\'s Republic of',
-  'Korea, Republic of',
-  'Kuwait',
-  'Kyrgyzstan',
-  'Lao People\'s Democratic Republic',
-  'Latvia',
-  'Lebanon',
-  'Lesotho',
-  'Liberia',
-  'Libya',
-  'Liechtenstein',
-  'Lithuania',
-  'Luxembourg',
-  'Macao',
-  'Macedonia, the former Yugoslav Republic of',
-  'Madagascar',
-  'Malawi',
-  'Malaysia',
-  'Maldives',
-  'Mali',
-  'Malta',
-  'Marshall Islands',
-  'Martinique',
-  'Mauritania',
-  'Mauritius',
-  'Mayotte',
-  'Mexico',
-  'Micronesia, Federated States of',
-  'Moldova, Republic of',
-  'Monaco',
-  'Mongolia',
-  'Montenegro',
-  'Montserrat',
-  'Morocco',
-  'Mozambique',
-  'Myanmar',
-  'Namibia',
-  'Nauru',
-  'Nepal',
-  'Netherlands',
-  'New Caledonia',
-  'New Zealand',
-  'Nicaragua',
-  'Niger',
-  'Nigeria',
-  'Niue',
-  'Norfolk Island',
-  'Northern Mariana Islands',
-  'Norway',
-  'Oman',
-  'Pakistan',
-  'Palau',
-  'Palestinian Territory, Occupied',
-  'Panama',
-  'Papua New Guinea',
-  'Paraguay',
-  'Peru',
-  'Philippines',
-  'Pitcairn',
-  'Poland',
-  'Portugal',
-  'Puerto Rico',
-  'Qatar',
-  'Réunion',
-  'Romania',
-  'Russian Federation',
-  'Rwanda',
-  'Saint Barthélemy',
-  'Saint Helena, Ascension and Tristan da Cunha',
-  'Saint Kitts and Nevis',
-  'Saint Lucia',
-  'Saint Martin (French part)',
-  'Saint Pierre and Miquelon',
-  'Saint Vincent and the Grenadines',
-  'Samoa',
-  'San Marino',
-  'Sao Tome and Principe',
-  'Saudi Arabia',
-  'Senegal',
-  'Serbia',
-  'Seychelles',
-  'Sierra Leone',
-  'Singapore',
-  'Sint Maarten (Dutch part)',
-  'Slovakia',
-  'Slovenia',
-  'Solomon Islands',
-  'Somalia',
-  'South Africa',
-  'South Georgia and the South Sandwich Islands',
-  'South Sudan',
-  'Spain',
-  'Sri Lanka',
-  'Sudan',
-  'Suriname',
-  'Svalbard and Jan Mayen',
-  'Swaziland',
-  'Sweden',
-  'Switzerland',
-  'Syrian Arab Republic',
-  'Taiwan, Province of China',
-  'Tajikistan',
-  'Tanzania, United Republic of',
-  'Thailand',
-  'Timor-Leste',
-  'Togo',
-  'Tokelau',
-  'Tonga',
-  'Trinidad and Tobago',
-  'Tunisia',
-  'Turkey',
-  'Turkmenistan',
-  'Turks and Caicos Islands',
-  'Tuvalu',
-  'Uganda',
-  'Ukraine',
-  'United Arab Emirates',
-  'United Kingdom',
-  'United States',
-  'United States Minor Outlying Islands',
-  'Uruguay',
-  'Uzbekistan',
-  'Vanuatu',
-  'Venezuela, Bolivarian Republic of',
-  'Viet Nam',
-  'Virgin Islands, British',
-  'Virgin Islands, U.S.',
-  'Wallis and Futuna',
-  'Western Sahara',
-  'Yemen',
-  'Zambia',
-  'Zimbabwe',
-];
-
-for (let i = 0; i < 248; i++) {
-  countryDropdown.push(
-    <MenuItem value={countries[i]} key={i} primaryText={countries[i]} />
-  );
-}
 
 class CheckoutForm extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       city: '',
+      deliverCity: '',
       name: '',
-      country: '',
-      value: 'United States',
-      countryList: '',
-      customerToken: 'user.id',
+      country: 'United States',
+      deliverCountry: 'United States',
+      password: '',
+      email: '',
+      value: 'Alabama',
+      deliverState: '',
+      deliverZipCode: '',
+      address: '',
+      deliverAddress: '',
+      plan: 'tierOne',
+      checkBox: false,
     };
   }
-
-
-  handleCountry = (event, index, value) => {
+  handleState = (event, index, value) => {
     this.setState({ value });
     this.setState({ country: value });
   };
+
+  handleDeliverState = (ev) => {
+    this.setState({ deliverState: ev.target.value });
+  }
 
   handleName = (ev) => {
     this.setState({ name: ev.target.value });
   }
 
+  handlePassword = (ev) => {
+    this.setState({ password: ev.target.value });
+  }
+
+  handleEmail = (ev) => {
+    this.setState({ email: ev.target.value });
+  }
+
   handleCity = (ev) => {
     this.setState({ city: ev.target.value });
+  }
+
+  handleDeliverCity = (ev) => {
+    this.setState({ deliverCity: ev.target.value });
+  }
+
+  handleBillingAddress = (ev) => {
+    this.setState({ address: ev.target.value });
+  }
+
+  handleDeliverAddress = (ev) => {
+    this.setState({ deliverAddress: ev.target.value });
+  }
+
+  handleDeliverZip = (ev) => {
+    this.setState({ deliverZipCode: ev.target.value });
   }
 
   handleSubmit = (ev) => {
@@ -307,96 +77,349 @@ class CheckoutForm extends React.PureComponent {
     this.props.stripe.createToken({
       name: this.state.name,
       address_city: this.state.city,
-      address_country: this.state.country
+      address_country: this.state.country,
+      address_line1: this.state.address,
+      address_state: this.state.value,
     }).then(({ token }) => {
-      console.log( JSON.stringify(token));
-      this.setState({ customerToken: JSON.stringify(token) });
-    });
-
-    // However, this line of code will do the same thing:
-    // this.props.stripe.createToken({type: 'card', name: 'Jenny Rosen'});
-  }
-  storeUser = () => {
-    const data = new FormData();
-    data.append('city', this.state.city);
-    data.append('country', this.state.country);
-    data.append('body', this.state.body);
-    data.append('address', this.state.address);
-    data.append('image2', this.state.image2);
-    fetch('http://jaspar.lamar.crab.com:8000/api/storeArticle?token='.concat(this.state.token), {
-      method: 'post',
-      body: data,
-      headers: { 'Authorization':'Bearer '.concat(this.state.token) }
-    }).then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      if (json.success) {
-        alert(json.success);
-      } else if (json.error) {
-        alert(json.error);
+      const data = new FormData();
+      data.append('username', this.state.name);
+      data.append('email', this.state.email);
+      data.append('password', this.state.password);
+      data.append('billingAddress', this.state.address);
+      data.append('billingZipCode', token.card.address_zip);
+      data.append('billingCity', this.state.city);
+      data.append('billingCountry', this.state.country);
+      data.append('useBillingAddress', this.state.checkBox);
+      data.append('customerToken', token.id);
+      data.append('plan', 'tierOne');
+      if (this.state.checkBox === false) {
+        data.append('deliverAddress', this.state.deliverAddress);
+        data.append('deliverZipCode', this.state.deliverZipCode);
+        data.append('deliverCity', this.state.deliverCity);
+        data.append('deliverCountry', this.state.deliverCountry);
       }
+      fetch('http://localhost:8000/api/signUp', {
+        method: 'post',
+        body: data,
+      }).then((response) => {
+        return response.json();
+      }).then((json) => {
+        if (json.success) {
+          alert(json.success);
+        } else if (json.error) {
+          alert(json.error);
+        }
+      });
     });
-  } 
+  }
 
+  handleSubmitTwo = (ev) => {
+    // We don't want to let default form submission happen here, which would refresh the page.
+    ev.preventDefault();
+
+    // Within the context of `Elements`, this call to createToken knows which Element to
+    // tokenize, since there's only one in this group.
+    this.props.stripe.createToken({
+      name: this.state.name,
+      address_city: this.state.city,
+      address_country: this.state.country,
+      address_line1: this.state.address,
+      address_state: this.state.value,
+    }).then(({ token }) => {
+      const data = new FormData();
+      data.append('username', this.state.name);
+      data.append('email', this.state.email);
+      data.append('password', this.state.password);
+      data.append('billingAddress', this.state.address);
+      data.append('billingZipCode', token.card.address_zip);
+      data.append('billingCity', this.state.city);
+      data.append('billingCountry', this.state.country);
+      data.append('useBillingAddress', this.state.checkBox);
+      data.append('customerToken', token.id);
+      data.append('plan', 'tierTwo');
+      if (this.state.checkBox === false) {
+        data.append('deliverAddress', this.state.deliverAddress);
+        data.append('deliverZipCode', this.state.deliverZipCode);
+        data.append('deliverCity', this.state.deliverCity);
+        data.append('deliverCountry', this.state.deliverCountry);
+      }
+      fetch('http://localhost:8000/api/signUp', {
+        method: 'post',
+        body: data,
+      }).then((response) => {
+        return response.json();
+      }).then((json) => {
+        if (json.success) {
+          alert(json.success);
+        } else if (json.error) {
+          alert(json.error);
+        }
+      });
+    });
+  }
+
+  handleSubmitThree = (ev) => {
+    // We don't want to let default form submission happen here, which would refresh the page.
+    ev.preventDefault();
+
+    // Within the context of `Elements`, this call to createToken knows which Element to
+    // tokenize, since there's only one in this group.
+    this.props.stripe.createToken({
+      name: this.state.name,
+      address_city: this.state.city,
+      address_country: this.state.country,
+      address_line1: this.state.address,
+      address_state: this.state.value,
+    }).then(({ token }) => {
+      const data = new FormData();
+      data.append('username', this.state.name);
+      data.append('email', this.state.email);
+      data.append('password', this.state.password);
+      data.append('billingAddress', this.state.address);
+      data.append('billingZipCode', token.card.address_zip);
+      data.append('billingCity', this.state.city);
+      data.append('billingCountry', this.state.country);
+      data.append('useBillingAddress', this.state.checkBox);
+      data.append('customerToken', token.id);
+      data.append('plan', 'tierThree');
+      if (this.state.checkBox === false) {
+        data.append('deliverAddress', this.state.deliverAddress);
+        data.append('deliverZipCode', this.state.deliverZipCode);
+        data.append('deliverCity', this.state.deliverCity);
+        data.append('deliverCountry', this.state.deliverCountry);
+      }
+      fetch('http://localhost:8000/api/signUp', {
+        method: 'post',
+        body: data,
+      }).then((response) => {
+        return response.json();
+      }).then((json) => {
+        if (json.success) {
+          alert(json.success);
+        } else if (json.error) {
+          alert(json.error);
+        }
+      });
+    });
+  }
+
+  handleCheckBox = () => {
+    if (this.state.checkBox === true) {
+      this.setState({ checkBox: false });
+    } else if (this.state.checkBox === false) {
+      this.setState({ checkBox: true });
+    }
+  }
+
+  handleDeliverAddress = () => {
+    if (this.state.checkBox === false) {
+      return (
+        <div style={{ marginTop: '10%' }}>
+          <h1>Delivery Address</h1>
+
+          <div style={{ marginTop: '5%' }}>
+            <form>
+              <SelectField onChange={this.handleDeliverState} maxHeight={200} menuStyle={{ background: 'white', height: '78%' }} value={this.state.value}>
+                {countryDropdown}
+              </SelectField>
+            </form>
+          </div>
+
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <form>
+
+              <input onChange={this.handleDeliverCity} placeholder=" Deliver City" />
+              <br />
+
+              <input onChange={this.handleDeliverAddress} placeholder=" Deliver Address" />
+              <br />
+
+              <input onChange={this.handleDeliverZip} placeholder=" Deliver Zipcode" />
+              <br />
+            </form>
+          </div>
+        </div>
+      );
+    }
+  }
   render() {
-    const styles = {
-      errorStyle: {
-        color: orange500,
-      },
-      underlineStyle: {
-        borderColor: orange500,
-      },
-      floatingLabelStyle: {
-        color: orange500,
-      },
-      floatingLabelFocusStyle: {
-        color: blue500,
-      },
+    const bodyStyle = {
+      height: '100%',
+      background: '#f6edeb',
+    };
+    const selectStyle = {
+      background: 'white',
+      height: '78%',
+    };
+    const formStyle = {
+      display: 'flex',
+      flexDirection: 'column',
+      fontSize: '1.9em',
+      fontWeight: 'bold',
+      marginBottom: '5%',
+    };
+    const belowDropDownStyle = {
+      display: 'flex',
+      flexDirection: 'column',
+    };
+    const buttonStyle = {
+      backgroundColor: '#4CAF50', /* Green */
+      border: 'none',
+      color: 'white',
+      padding: '15px 32px',
+      textAlign: 'center',
+      textDecoration: 'none',
+      display: 'inline-block',
+      fontSize: '16px',
+    };
+    const tierStyle = {
+      display: 'flex',
+      flexDirection: 'column',
     };
 
     return (
-      <div>
-        <SelectField
-          value={this.state.value}
-          onChange={this.handleCountry}
-          maxHeight={200}
-        >
-          {countryDropdown}
-        </SelectField>
-        <TextField
-          onChange={this.handleName}
-          hintText="Name"
-          hintStyle={styles.errorStyle}
-        /><br />
-        <TextField
-          onChange={this.handleCity}
-          hintText="City"
-          errorText="This field is required."
-          errorStyle={styles.errorStyle}
-        /><br />
-        <TextField
-          hintText="Custom Underline Color"
-          underlineStyle={styles.underlineStyle}
-        /><br />
-        <TextField
-          hintText="Custom Underline Focus Color"
-          underlineFocusStyle={styles.underlineStyle}
-        /><br />
-        <TextField
-          floatingLabelText="Styled Floating Label Text"
-          floatingLabelStyle={styles.floatingLabelStyle}
-          floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
-        />
-        <form >
-          {/* <AddressSection /> */}
-          <CardSection />
-          <RaisedButton onTouchTap={this.handleSubmit} label="Submit" primary />
-        </form>
-        <a href="http://localhost:8000/checkout">helloss</a>
+      <div style={bodyStyle}>
+        <div style={formStyle}>Contacts Info</div>
+        <div>
+          <form>
+
+            <div style={tierStyle}>
+              Name
+              <input type="text" onChange={this.handleName} placeholder="name" />
+              <br />
+            </div>
+
+            <div style={tierStyle}>
+              Email
+              <input onChange={this.handleEmail} placeholder="Email" />
+              <br />
+            </div>
+
+            <div style={tierStyle}>
+              Password
+              <input type="text" onChange={this.handlePassword} placeholder="password" />
+              <br />
+            </div>
+
+          </form>
+        </div>
+
+        <div>
+          <div style={formStyle}>Billing Address</div>
+          <form>
+            <SelectField menuStyle={selectStyle} value={this.state.value} onChange={this.handleState} maxHeight={200}>
+              {countryDropdown}
+            </SelectField>
+          </form>
+        </div>
+
+        <div style={belowDropDownStyle}>
+          <form>
+
+            <div style={tierStyle}>
+              City
+              <input onChange={this.handleCity} placeholder="City" />
+              <br />
+            </div>
+
+            <div style={tierStyle}>
+              Address
+              <input onChange={this.handleBillingAddress} placeholder="Address" />
+              <br />
+            </div>
+
+          </form>
+
+          <form>
+            Credit Card Information
+            <CardSection />
+          </form>
+
+        </div>
+
+        Use billing address as shipping address?
+
+        <input type="checkbox" onChange={this.handleCheckBox} text="Submit" />
+        {this.handleDeliverAddress()}
+
+        <div style={tierStyle}>
+          Tier One
+          <button style={buttonStyle} onTouchTap={this.handleSubmit}>
+            Submit
+          </button>
+
+          TierTwo
+          <button style={buttonStyle} onTouchTap={this.handleSubmitTwo}>
+            Submit
+          </button>
+
+          TierThree
+          <button style={buttonStyle} onTouchTap={this.handleSubmitThree}>
+            Submit
+          </button>
+        </div>
       </div>
     );
   }
 }
-export default injectStripe(CheckoutForm);
+const countryDropdown = [];
+const states = [
+  'Alabama',
+  'Alaska',
+  'Arizona',
+  'Arkansas',
+  'California',
+  'Colorado',
+  'Connecticut',
+  'Delaware',
+  'Florida',
+  'Georgia',
+  'Hawaii',
+  'Idaho',
+  'Illinois',
+  'Indiana',
+  'Iowa',
+  'Kansas',
+  'Kentucky',
+  'Louisiana',
+  'Maine',
+  'Maryland',
+  'Massachusetts',
+  'Michigan',
+  'Minnesota',
+  'Mississippi',
+  'Missouri',
+  'Montana',
+  'Nebraska',
+  'Nevada',
+  'New Hampshire',
+  'New Jersey',
+  'New Mexico',
+  'New York',
+  'North Carolina',
+  'North Dakota',
+  'Ohio',
+  'Oklahoma',
+  'Oregon',
+  'Pennsylvania',
+  'Rhode Island',
+  'South Carolina',
+  'South Dakota',
+  'Tennessee',
+  'Texas',
+  'Utah',
+  'Vermont',
+  'Virginia',
+  'Washington',
+  'West Virginia',
+  'Wisconsin',
+  'Wyoming',
+];
 
+for (let i = 0; i < 50; i++) {
+  countryDropdown.push(
+    <MenuItem value={states[i]} key={i} primaryText={states[i]} />
+  );
+}
+export default injectStripe(CheckoutForm);
 
