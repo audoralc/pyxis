ace3f854ba99d189e090434ffa8c9b1f6d767aeb
Audora LeeAnn <scribeklio@gmail.com>
2017-06-12 15:46:43 -0400

    subscribe pg started

--- app/containers/Subscribe/index.js
+++ app/containers/Subscribe/index.js
@@ -12,7 +12,11 @@ import Footer from 'components/Footer';
 import glamorous from 'glamorous'; 
 import { FaDollar } from 'react-icons/lib/fa'; 
 
+const TierDiv = glamorous.div({ 
+  background: '#FAFAFA',
 
+ 
+})
 
 export default class Subscribe extends React.PureComponent {
   render() {
@@ -20,16 +24,45 @@ export default class Subscribe extends React.PureComponent {
     const pageGrid = { 
       display: 'grid',
       gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr', 
-      gridTemplateRows: '1fr 50vh auto auto 50vh 10%',
+      gridTemplateRows: '1fr 50vh 50vh auto auto 10%',
     }
+ 
+        const titleBlock = {
+          textAlign: 'center' ,
+        }
+
+        const tierGroup ={    
+          display: 'flex', 
+          flexDirection: 'row', 
+          margin: '5%',
+          justifyContent: 'space-around', 
+        }
     
     return (
       <div style={pageGrid}>
         <Helmet title="Subscribe" meta={[ { name: 'description', content: 'Description of Cart' }]}/>
          <Header></Header>
 
-        <BodyContainer> 
-      
+        <BodyContainer>
+        
+          <h2 style={titleBlock}> Pick a size: </h2>
+          <section style={tierGroup}>            
+            <TierDiv>               
+              <div><FaDollar></FaDollar></div> 
+              <h3>Tier One</h3>
+            </TierDiv>
+
+            <TierDiv> 
+              <div><FaDollar></FaDollar><FaDollar></FaDollar></div> 
+              <h3>Tier Two</h3>
+            </TierDiv>
+
+            <TierDiv> 
+              <div><FaDollar></FaDollar><FaDollar></FaDollar><FaDollar></FaDollar></div> 
+              <h3>Tier Three</h3>
+            </TierDiv>
+           </section>
+       
         </BodyContainer>
 
         <Footer></Footer> 
