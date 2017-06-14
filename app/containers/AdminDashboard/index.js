c85fe3126f6b02ca49ecefee5ba15a29df735c86
Oscar-21 <austin.conder@outlook.com>
2017-06-14 10:56:51 -0400

    updated Admin page

--- app/containers/AdminDashboard/index.js
+++ app/containers/AdminDashboard/index.js
@@ -5,75 +5,55 @@
  */
 import React from 'react';
 import Helmet from 'react-helmet';
-import AppBar from 'material-ui/AppBar';
+import UsersTable from 'components/UsersTable';
 import RaisedButton from 'material-ui/RaisedButton';
-import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
-
 
 export default class AdminDashboard extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       token: sessionStorage.getItem('token'),
-      users: [],
+      role: sessionStorage.getItem('role'),
     };
   }
-
-  componentWillMount() {
-    fetch('http://localhost:8000/api/showUsers', {
-      headers:{'Authorization': 'Bearer ' + this.state.token }
-    }).then((response) => {
-      return response.json();
-    }).then((json) => {
-      this.setState({ users: json });
-      console.log(users);
-    });
+  componentWillMount()
+  {
+                
   }
-
+  handleInvoice = () => {
+    if (this.state.loggedIn === true) {
+      return (
+        <a href="http://localhost:8000/api/inv">
+          <RaisedButton
+            primary
+            label="Invoice"
+          />
+        </a>
+      );
+    }
+  }
   render() {
     return (
       <div>
-        <Helmet title="AdminDashboard" meta={[ { name: 'description', content: 'Description of AdminDashboard' }]}/>
-        <AppBar title="Title" iconClassNameRight="muidocs-icon-navigation-expand-more" />
-        <Table>
-    <TableHeader>
-      <TableRow>
-        <TableHeaderColumn>id</TableHeaderColumn>
-        <TableHeaderColumn>name</TableHeaderColumn>
-        <TableHeaderColumn>email</TableHeaderColumn>
-        <TableHeaderColumn>tier</TableHeaderColumn>
-        <TableHeaderColumn>stripe_plan</TableHeaderColumn>
-        <TableHeaderColumn>quantity</TableHeaderColumn>
-        <TableHeaderColumn>roleId</TableHeaderColumn>
-        <TableHeaderColumn>Country</TableHeaderColumn>
-        <TableHeaderColumn>City</TableHeaderColumn>
-        <TableHeaderColumn>ends_at</TableHeaderColumn>
-        <TableHeaderColumn>created_at</TableHeaderColumn>
-        <TableHeaderColumn>updated_at</TableHeaderColumn>
-      </TableRow>
-    </TableHeader>
+        <Helmet title="AdminDashboard" meta={[{ name: 'description', content: 'Description of AdminDashboard' }]} />
+        <UsersTable />
+        <div>
+          {this.handleInvoice()}
+          <RaisedButton
+            primary
+            label="Check"
+            onTouchTap={this.handleCheck}
+          />
 
-    <TableBody>
-
-{this.state.users.map((user, index) => (
-      <TableRow>
-        <TableHeaderColumn>{user.id}</TableHeaderColumn>
-        <TableHeaderColumn>{user.name}</TableHeaderColumn>
-        <TableHeaderColumn>{user.email}</TableHeaderColumn>
-        <TableHeaderColumn>{user.tier}</TableHeaderColumn>
-        <TableHeaderColumn>{user.stripe_plan}</TableHeaderColumn>
-        <TableHeaderColumn>{user.quantity}</TableHeaderColumn>
-        <TableHeaderColumn>{user.roleId}</TableHeaderColumn>
-        <TableHeaderColumn>{user.billingCountry}</TableHeaderColumn>
-        <TableHeaderColumn>{user.billingCity}</TableHeaderColumn>
-        <TableHeaderColumn>{user.ends_at}</TableHeaderColumn>
-        <TableHeaderColumn>{user.created_at}</TableHeaderColumn>
-        <TableHeaderColumn>{user.updated_at}</TableHeaderColumn>
-      </TableRow>
-))}
-    </TableBody>
-  </Table>
+          <a href="http://localhost:8000/api/lout">
+            <RaisedButton
+              primary
+              label="out"
+            />
+          </a>
+        </div>
       </div>
     );
   }
 }
+
